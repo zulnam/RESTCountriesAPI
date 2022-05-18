8530d883522ec14147247fcaaff4c27e51d1da37
Valentin Necsescu <valentin.necsescualpima.com@valentins-macbook-pro-1.home>
2022-05-18 11:58:07 +0200

    feature/added search by name

--- src/components/Filters/SearchInput.js
+++ src/components/Filters/SearchInput.js
@@ -1,5 +1,8 @@
+import { useEffect, useState, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from '@emotion/styled';
 import Image from 'next/image';
+import { searchForCountry } from '../../store/reducers/countries/countriesReducer';
 
 const InputContainer = styled.div`
   height: 3.5rem;
@@ -25,18 +28,42 @@ const IconContainer = styled.span`
   margin-left: 1rem;
 `;
 
-const SearchInput = () => (
-  <InputContainer>
-    <IconContainer>
-      <Image
-        src="/magnifying-glass.svg"
-        width={16}
-        height={16}
-        alt="Search Icon"
+const SearchInput = () => {
+  const dispatch = useDispatch();
+  const regionFilter = useSelector((state) => state.countries.filters.region);
+  const [value, setValue] = useState('');
+
+  const changeHandler = useCallback((event) => {
+    setValue(event.target.value);
+  }, []);
+
+  useEffect(() => {
+    dispatch(searchForCountry(value));
+  }, [dispatch, value]);
+
+  useEffect(() => {
+    setValue('');
+  }, [regionFilter]);
+
+  return (
+    <InputContainer>
+      <IconContainer>
+        <Image
+          src="/magnifying-glass.svg"
+          width={16}
+          height={16}
+          alt="Search Icon"
+        />
+      </IconContainer>
+      <input
+        type="text"
+        name="search"
+        placeholder="Search for a country..."
+        value={value}
+        onChange={changeHandler}
       />
-    </IconContainer>
-    <input type="text" name="search" placeholder="Search for a country..." />
-  </InputContainer>
-);
+    </InputContainer>
+  );
+};
 
 export default SearchInput;
