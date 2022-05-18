9d9f2d9a69dfc3fcee5ee924c36f04df2adc97ea
Valentin Necsescu <valentin.necsescualpima.com@valentins-macbook-pro-1.home>
2022-05-18 09:35:40 +0200

    ref/removed unused imports from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,3 @@
-import { useEffect, useState } from 'react';
-import { getCountries } from './api/hello';
-import { useDispatch, useSelector } from 'react-redux';
-import { setRawCountries } from '../store/reducers/countries/countriesReducer';
-
 import CountriesGrid from '../components/Countries/CountriesGrid';
 import Filters from '../components/Filters/Filters';
 
