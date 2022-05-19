1a975ce2a4be8e35d653ab7e30181a1f16d07775
Valentin Necsescu <valentin.necsescualpima.com@valentins-macbook-pro-2.home>
2022-05-19 16:20:02 +0200

    store/added user reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import countriesReducer from './reducers/countries/countriesReducer';
+import userReducer from './reducers/user/userReducer';
 
 const rootReducer = {
   countries: countriesReducer,
+  user: userReducer,
 };
 
 const store = configureStore({
