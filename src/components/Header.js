a0c8fecbcfc9c3e4bc21e6b1dd443849cd48153f
Valentin Necsescu <valentin.necsescualpima.com@valentins-macbook-pro-2.home>
2022-05-19 10:15:55 +0200

    styling/added responsive styles to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,34 @@
 import styled from '@emotion/styled';
+import theme from '../styles/theme';
 
 const HeaderContainer = styled.header`
   display: flex;
-  width: 100%;
   justify-content: space-between;
   align-items: center;
-  background-color: white;
-  color: rgba(32, 44, 55, 1);
-  padding: 0 42px;
-  margin-bottom: 42px;
+  padding: 0 10px;
+  margin-bottom: 20px;
   height: 80px;
+  width: 100%;
 
-  h1 {
-    font-weight: 800;
-    font-size: 2rem;
+  h1,
+  span {
+    font-size: 0.9rem;
+  }
+  @media (min-width: ${theme.breakpoints.md}) {
+    padding: 0 42px;
+    margin-bottom: 42px;
+
+    h1 {
+      font-weight: 800;
+      font-size: 2rem;
+    }
   }
 `;
 
 const Header = () => (
-  <HeaderContainer>
+  <HeaderContainer className="white-theme">
     <h1>Where in the world?</h1>
-    <span>Toggle Placeholder</span>
+    <span>Dark Mode</span>
   </HeaderContainer>
 );
 
